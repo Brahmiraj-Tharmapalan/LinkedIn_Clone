644fa0166066867d9d502c2864382aa578c22a75
Brahmiraj-Tharmapalan <123236415+Brahmiraj-Tharmapalan@users.noreply.github.com>
2023-07-27 20:41:51 +0530

    update

--- Frontend/src/Header/Header.jsx
+++ Frontend/src/Header/Header.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
-import LinkedIn_logo from '../assets/LinkedIn_logo.png'
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import LinkedIn_logo from "../assets/LinkedIn_logo.png";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
     <div className="header">
       <div className="header_left">
+        <img src={LinkedIn_logo} alt="" />
         <div className="header_search">
-            <img src={LinkedIn_logo} alt="" />
           <SearchIcon />
           <input type="text" />
         </div>
       </div>
 
-      <div className="header_right"></div>
+      <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title = "Home"/>
+        <HeaderOption Icon={SupervisorAccountIcon} title = "My Network"/>
+
+      </div>
     </div>
   );
 }
